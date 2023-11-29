be5feddba06619c36da2659235e5cc5107221f9a
Kauê José <134783875+Kaue404@users.noreply.github.com>
2023-11-29 20:08:58 -0300

    Update conexao2.php

--- conexao2.php
+++ conexao2.php
@@ -1,9 +1,9 @@
 <?php
 
 $hostname ='localhost';
-$username ='root';
-$banco ='site1';
-$senha ='';
+$username ='dese0964_site1';
+$banco ='dese0964_site1';
+$senha ='si@171156';
 
 $db = mysqli_connect($hostname,$username,$senha,$banco) or die (mysqli_error());
 mysqli_select_db($db, $banco);
